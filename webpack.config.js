1f45199a09b9658ac396877d903f5fb7ff2067d7
hannah <contactuser2220@gmail.com>
2020-11-24 18:20:46 -0500

    changed webpack and webpack cli versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-"use strict";
+const isDev = process.env.NODE_ENV === "development";
 
 const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
+  mode: isDev ? "development" : "production",
   entry: ["@babel/polyfill", "./src/index.js"],
   output: {
     path: path.resolve(__dirname, "public"),
