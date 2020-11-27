b018080137d39447e132cedb232634b70a9b4006
Catalina McQuade <catalinamcquade@gmail.com>
2020-11-27 14:21:27 -0500

    initialized RegexScene

---
+++ src/scenes/RegexScene.js
@@ -0,0 +1,15 @@
+import Phaser from "phaser";
+
+export default class RegexScene extends Phaser.Scene {
+    constructor() {
+        super("RegexScene");
+    }
+
+    preload() {}
+
+    create() {
+
+    }
+
+    update() {}
+}
