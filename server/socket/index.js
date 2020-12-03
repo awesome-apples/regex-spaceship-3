4fb1295792cb5706bdbdc045f6c9da8cf341c2c6
hannah <contactuser2220@gmail.com>
2020-12-03 15:28:44 -0500

    feat: multi game room WIP

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,49 +1,98 @@
 const { Task } = require("../db/models");
 
-let serverState = {
-  users: [],
-  randomTasks: [],
-  scores: [],
-  gameScore: 0,
+const gameRooms = {
+  // [roomKey]: {
+  //     users: [],
+  //     randomTasks: [],
+  //     scores: [],
+  //     gameScore: 0,
+  //     players: {},
+  //     numPlayers: 0
+  // }
 };
 
-const players = {};
-let numPlayers = 0;
+
+
+//write join room
+
+
+
+//hash table
+//key is the gameroom id
+// it holds and object with the games state
+// const gameRooms= [{gameScore: , roomId: , players, numPlayers}]
 
 module.exports = (io) => {
   io.on("connection", (socket) => {
-    console.log(numPlayers);
     console.log(
       `A socket connection to the server has been made: ${socket.id}`
     );
-    players[socket.id] = {
-      rotation: 0,
-      x: Math.floor(Math.random() * 700) + 50,
-      y: Math.floor(Math.random() * 500) + 50,
-      playerId: socket.id,
-      team: Math.floor(Math.random() * 2) == 0 ? "red" : "blue",
-    };
-    numPlayers = Object.keys(players).length;
-    console.log(numPlayers);
-    // send the players object to the new player
-    socket.emit("currentPlayers", { players, numPlayers });
-    // set initial state
-    socket.emit("setState", serverState);
-    // update all other players of the new player
-    socket.broadcast.emit("newPlayer", {
-      playerInfo: players[socket.id],
-      numPlayers: numPlayers,
+    socket.on("joinRoom", (arg) => {
+      const { roomKey, socket } = arg;
+      const roomInfo = (gameRooms[roomKey] = {});
+      console.log(roomInfo.numPlayers);
+      roomInfo.players[socket.id] = {
+        rotation: 0,
+        x: Math.floor(Math.random() * 700) + 50,
+        y: Math.floor(Math.random() * 500) + 50,
+        playerId: socket.id,
+        team: Math.floor(Math.random() * 2) == 0 ? "red" : "blue",
+      };
+      roomInfo.numPlayers = Object.keys(roomInfo.players).length;
+      console.log(roomInfo.numPlayers);
+      // send the players object to the new player
+      socket.emit("currentPlayers", {
+        players: roomInfo.players,
+        numPlayers: roomInfo.numPlayers,
+      });
+      // set initial state
+      socket.emit("setState", roomInfo);
+      // update all other players of the new player
+      socket.broadcast.emit("newPlayer", {
+        playerInfo: roomInfo.players[socket.id],
+        numPlayers: roomInfo.numPlayers,
+      });
     });
+
+
+    // const gameRooms = {
+    //   // [roomKey]: {
+    //   //     users: [],
+    //   //     randomTasks: [],
+    //   //     scores: [],
+    //   //     gameScore: 0,
+    //   //     players: {},
+    //   //     numPlayers: 0
+    //   // }
+    // };
+
+
     // when a player disconnects, remove them from our players object
     socket.on("disconnect", function () {
+
+      //find which room they belong to
+      let roomKey = 0
+
+      for (let room in gameRooms) {
+        for(let socketId in room.players) {
+          if (socketId === socket.id){
+            roomKey = room
+          }
+      }
+
+      // const roomInfo = gameRooms[roomKey];
       console.log("user disconnected: ", socket.id);
       // remove this player from our players object
-      delete players[socket.id];
-      numPlayers = Object.keys(players).length;
-      console.log(numPlayers);
+      delete roomInfo.players[socket.id];
+      numPlayers = Object.keys(roomInfo.players).length;
+      console.log(roomInfo.numPlayers);
       // emit a message to all players to remove this player
-      io.emit("disconnected", { playerId: socket.id, numPlayers });
+      io.to(roomKey).emit("disconnected", {
+        playerId: socket.id,
+        numPlayers: roomInfo.numPlayers,
+      });
     });
+
     // when a player moves, update the player data
     socket.on("playerMovement", function (movementData) {
       players[socket.id].x = movementData.x;
