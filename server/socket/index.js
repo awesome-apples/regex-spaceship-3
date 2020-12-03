eeab39bfd2865f1d243f38706c50323fb43b2e6c
HopeFourie <hopefourie@gmail.com>
2020-12-03 16:40:01 -0500

    fix: refactored sockets to accomodate rooms

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,4 +1,4 @@
-const { Task } = require("../db/models");
+const { Task } = require('../db/models');
 
 const gameRooms = {
   // [roomKey]: {
@@ -11,11 +11,7 @@ const gameRooms = {
   // }
 };
 
-
-
 //write join room
-
-
 
 //hash table
 //key is the gameroom id
@@ -23,12 +19,13 @@ const gameRooms = {
 // const gameRooms= [{gameScore: , roomId: , players, numPlayers}]
 
 module.exports = (io) => {
-  io.on("connection", (socket) => {
+  io.on('connection', (socket) => {
     console.log(
       `A socket connection to the server has been made: ${socket.id}`
     );
-    socket.on("joinRoom", (arg) => {
+    socket.on('joinRoom', (arg) => {
       const { roomKey, socket } = arg;
+      socket.join(roomKey);
       const roomInfo = (gameRooms[roomKey] = {});
       console.log(roomInfo.numPlayers);
       roomInfo.players[socket.id] = {
@@ -36,97 +33,88 @@ module.exports = (io) => {
         x: Math.floor(Math.random() * 700) + 50,
         y: Math.floor(Math.random() * 500) + 50,
         playerId: socket.id,
-        team: Math.floor(Math.random() * 2) == 0 ? "red" : "blue",
+        team: Math.floor(Math.random() * 2) == 0 ? 'red' : 'blue',
       };
       roomInfo.numPlayers = Object.keys(roomInfo.players).length;
       console.log(roomInfo.numPlayers);
       // send the players object to the new player
-      socket.emit("currentPlayers", {
+      socket.emit('currentPlayers', {
         players: roomInfo.players,
         numPlayers: roomInfo.numPlayers,
       });
       // set initial state
-      socket.emit("setState", roomInfo);
+      socket.emit('setState', roomInfo);
       // update all other players of the new player
-      socket.broadcast.emit("newPlayer", {
+      socket.broadcast.emit('newPlayer', {
         playerInfo: roomInfo.players[socket.id],
         numPlayers: roomInfo.numPlayers,
       });
     });
-
-
-    // const gameRooms = {
-    //   // [roomKey]: {
-    //   //     users: [],
-    //   //     randomTasks: [],
-    //   //     scores: [],
-    //   //     gameScore: 0,
-    //   //     players: {},
-    //   //     numPlayers: 0
-    //   // }
-    // };
-
-
     // when a player disconnects, remove them from our players object
-    socket.on("disconnect", function () {
-
+    socket.on('disconnect', function () {
       //find which room they belong to
-      let roomKey = 0
-
-      for (let room in gameRooms) {
-        for(let socketId in room.players) {
-          if (socketId === socket.id){
-            roomKey = room
+      let roomKey = 0;
+      for (let keys1 in gameRooms) {
+        for (let keys2 in gameRooms[keys1]) {
+          for (let keys3 in gameRooms[keys1][keys2]) {
+            Object.keys(gameRooms[keys1][keys2]).map((el) => {
+              if (el === socket.id) {
+                roomKey = keys1;
+              }
+            });
           }
+        }
       }
-
-      // const roomInfo = gameRooms[roomKey];
-      console.log("user disconnected: ", socket.id);
+      const roomInfo = gameRooms[roomKey];
+      console.log('user disconnected: ', socket.id);
       // remove this player from our players object
       delete roomInfo.players[socket.id];
       numPlayers = Object.keys(roomInfo.players).length;
       console.log(roomInfo.numPlayers);
       // emit a message to all players to remove this player
-      io.to(roomKey).emit("disconnected", {
+      io.to(roomKey).emit('disconnected', {
         playerId: socket.id,
         numPlayers: roomInfo.numPlayers,
       });
     });
 
     // when a player moves, update the player data
-    socket.on("playerMovement", function (movementData) {
-      players[socket.id].x = movementData.x;
-      players[socket.id].y = movementData.y;
-      players[socket.id].rotation = movementData.rotation;
+    socket.on('playerMovement', function (data) {
+      const { x, y, roomKey } = data;
+      gameRooms[roomKey].players[socket.id].x = x;
+      gameRooms[roomKey].players[socket.id].y = y;
       // emit a message to all players about the player that moved
-      socket.broadcast.emit("playerMoved", players[socket.id]);
+      socket
+        .to(roomKey)
+        .emit('playerMoved', gameRooms[roomKey].players[socket.id]);
     });
-    socket.on("completedTask", function (completedTaskId) {
-      serverState.gameScore++;
-      io.emit("scoreUpdate", {
-        gameScore: serverState.gameScore,
-        completedTaskId,
+    socket.on('completedTask', function (data) {
+      const { roomKey } = data;
+      gameRooms[roomKey].gameScore++;
+      io.to(roomKey).emit('scoreUpdate', {
+        gameScore: gameRooms[roomKey].gameScore,
       });
     });
 
-    socket.on("startGame", async function () {
+    socket.on('startGame', async function (roomKey) {
       try {
         const tasks = await Task.findAll();
         const randomIdOne = Math.ceil(Math.random() * tasks.length);
         const randomIdTwo = Math.ceil(Math.random() * tasks.length);
         const taskOne = await Task.findByPk(randomIdOne);
         const taskTwo = await Task.findByPk(randomIdTwo);
-        serverState.randomTasks = [taskOne, taskTwo];
+        gameRooms[roomKey].randomTasks = [taskOne, taskTwo];
 
-        io.emit("updateState", serverState);
-        io.emit("destroyButton");
-        io.emit("startTimer");
+        io.to(roomKey).emit('updateState', gameRooms[roomKey]);
+        io.to(roomKey).emit('destroyButton');
+        io.to(roomKey).emit('startTimer');
       } catch (err) {
-        console.log("error starting game", err);
+        console.log('error starting game', err);
       }
     });
-    socket.on("disablePanel", function (controlPanel) {
-      socket.broadcast.emit("setInactive", controlPanel);
+    socket.on('disablePanel', function (data) {
+      const { controlPanel, roomKey } = data;
+      socket.to(roomKey).emit('setInactive', controlPanel);
     });
   });
 };
--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1,37 +1,42 @@
-import Phaser from "phaser";
-import ProgressBar from "../entity/progressBar";
-import ControlPanel from "../entity/ControlPanel";
+import Phaser from 'phaser';
+import ProgressBar from '../entity/progressBar';
+import ControlPanel from '../entity/ControlPanel';
 
 export default class MainScene extends Phaser.Scene {
   constructor() {
-    super("MainScene");
-    this.state = { users: [], randomTasks: [], scores: [], gameScore: 0 };
+    super('MainScene');
+    this.state = {
+      roomKey: '',
+      randomTasks: [],
+      gameScore: 0,
+      players: {},
+      numPlayers: 0,
+    };
     this.hasBeenSet = false;
     this.startClickable = true;
     this.beginTimer = false;
-    this.numPlayers = 0;
   }
 
   preload() {
-    this.load.spritesheet("astronaut", "assets/spritesheets/astronaut3.png", {
+    this.load.spritesheet('astronaut', 'assets/spritesheets/astronaut3.png', {
       frameWidth: 29,
       frameHeight: 37,
     });
-    this.load.image("controlPanelLeft", "assets/sprites/console_s.png");
-    this.load.image("controlPanelRight", "assets/sprites/console_w.png");
-    this.load.image("star", "assets/star_gold.png");
-    this.load.image("mainroom", "assets/backgrounds/mainroom.png");
+    this.load.image('controlPanelLeft', 'assets/sprites/console_s.png');
+    this.load.image('controlPanelRight', 'assets/sprites/console_w.png');
+    this.load.image('star', 'assets/star_gold.png');
+    this.load.image('mainroom', 'assets/backgrounds/mainroom.png');
   }
 
   async create() {
     const scene = this;
 
-    this.add.image(0, 0, "mainroom").setOrigin(0);
+    this.add.image(0, 0, 'mainroom').setOrigin(0);
 
     //PROGRESS BAR
-    this.progressText = this.add.text(30, 16, "Progress Tracker", {
-      fontSize: "20px",
-      fill: "#ffffff",
+    this.progressText = this.add.text(30, 16, 'Progress Tracker', {
+      fontSize: '20px',
+      fill: '#ffffff',
     });
 
     scene.progressBar = new ProgressBar(scene, 30, 50);
@@ -42,7 +47,7 @@ export default class MainScene extends Phaser.Scene {
       this.otherPlayers = this.physics.add.group();
       if (!this.hasBeenSet) {
         this.hasBeenSet = true;
-        this.socket.on("setState", function (state) {
+        this.socket.on('setState', function (state) {
           const { users, randomTasks, scores, gameScore } = state;
           scene.state.users = users;
           scene.state.randomTasks = randomTasks;
@@ -51,14 +56,14 @@ export default class MainScene extends Phaser.Scene {
         });
       }
 
-      this.socket.on("updateState", function (serverState) {
+      this.socket.on('updateState', function (serverState) {
         scene.state = serverState;
         scene.progressBar.changeTaskAmount(scene.state.randomTasks.length);
       });
 
-      this.socket.on("currentPlayers", function (arg) {
+      this.socket.on('currentPlayers', function (arg) {
         const { players, numPlayers } = arg;
-        scene.numPlayers = numPlayers;
+        scene.state.numPlayers = numPlayers;
         Object.keys(players).forEach(function (id) {
           if (players[id].playerId === scene.socket.id) {
             scene.addPlayer(scene, players[id]);
@@ -68,15 +73,15 @@ export default class MainScene extends Phaser.Scene {
         });
       });
 
-      this.socket.on("newPlayer", function (arg) {
+      this.socket.on('newPlayer', function (arg) {
         const { playerInfo, numPlayers } = arg;
         scene.addOtherPlayers(scene, playerInfo);
-        scene.numPlayers = numPlayers;
+        scene.state.numPlayers = numPlayers;
       });
 
-      this.socket.on("disconnected", function (arg) {
+      this.socket.on('disconnected', function (arg) {
         const { playerId, numPlayers } = arg;
-        scene.numPlayers = numPlayers;
+        scene.state.numPlayers = numPlayers;
         scene.otherPlayers.getChildren().forEach(function (otherPlayer) {
           if (playerId === otherPlayer.playerId) {
             otherPlayer.destroy();
@@ -84,7 +89,7 @@ export default class MainScene extends Phaser.Scene {
         });
       });
 
-      this.socket.on("playerMoved", function (playerInfo) {
+      this.socket.on('playerMoved', function (playerInfo) {
         scene.otherPlayers.getChildren().forEach(function (otherPlayer) {
           if (playerInfo.playerId === otherPlayer.playerId) {
             otherPlayer.setRotation(playerInfo.rotation);
@@ -94,22 +99,14 @@ export default class MainScene extends Phaser.Scene {
       });
       this.cursors = this.input.keyboard.createCursorKeys();
 
-      this.socket.on("scoreUpdate", function (arg) {
-        const { completedTaskId, gameScore } = arg;
-        for (let i = 0; i < scene.state.randomTasks.length; i++) {
-          if (scene.state.randomTasks[i].id === completedTaskId) {
-            scene.state.randomTasks[i].completed = true;
-          }
-        }
+      this.socket.on('scoreUpdate', function (arg) {
+        const { gameScore } = arg;
         scene.progressBar.increase(gameScore - scene.state.gameScore);
         scene.state.gameScore = gameScore;
         if (scene.state.gameScore >= scene.state.randomTasks.length) {
-          scene.scene.stop("RegexScene");
-          scene.scene.launch("WinScene", {
-            users: scene.state.users,
-            randomTasks: scene.state.randomTasks,
-            scores: scene.state.scores,
-            gameScore: scene.state.gameScore,
+          scene.scene.stop('RegexScene');
+          scene.scene.launch('WinScene', {
+            ...scene.state,
             socket: scene.socket,
           });
           scene.finalTime = scene.initialTime;
@@ -121,21 +118,21 @@ export default class MainScene extends Phaser.Scene {
         this,
         200,
         200,
-        "controlPanelLeft"
+        'controlPanelLeft'
       );
 
       this.controlPanelRight = new ControlPanel(
         this,
         580,
         400,
-        "controlPanelRight"
+        'controlPanelRight'
       );
 
-      this.socket.on("setInactive", function (controlPanel) {
-        if (controlPanel === "left") {
+      this.socket.on('setInactive', function (controlPanel) {
+        if (controlPanel === 'left') {
           scene.controlPanelLeft.disableInteractive();
           scene.controlPanelLeft.setTint(0xd86969);
-        } else if (controlPanel === "right") {
+        } else if (controlPanel === 'right') {
           scene.controlPanelRight.disableInteractive();
           scene.controlPanelRight.setTint(0xd86969);
         }
@@ -143,28 +140,26 @@ export default class MainScene extends Phaser.Scene {
 
       // click on control panels and Regex Scene will launch
 
-      this.controlPanelLeft.on("pointerdown", () => {
-        this.scene.launch("RegexScene", {
-          users: this.state.users,
-          randomTasks: this.state.randomTasks,
-          randomTask: this.state.randomTasks[0],
-          scores: this.state.scores,
-          gameScore: this.state.gameScore,
-          socket: this.socket,
+      this.controlPanelLeft.on('pointerdown', () => {
+        this.scene.launch('RegexScene', {
+          ...scene.state,
+          socket: scene.socket,
         });
-        scene.socket.emit("disablePanel", "left");
+        scene.socket.emit('disablePanel', {
+          controlPanel: 'left',
+          roomKey: scene.roomKey,
+        });
       });
 
-      this.controlPanelRight.on("pointerdown", () => {
-        this.scene.launch("RegexScene", {
-          users: this.state.users,
-          randomTasks: this.state.randomTasks,
-          randomTask: this.state.randomTasks[1],
-          scores: this.state.scores,
-          gameScore: this.state.gameScore,
-          socket: this.socket,
+      this.controlPanelRight.on('pointerdown', () => {
+        this.scene.launch('RegexScene', {
+          ...scene.state,
+          socket: scene.socket,
         });
-        scene.socket.emit("disablePanel", "right");
+        scene.socket.emit('disablePanel', {
+          controlPanel: 'right',
+          roomKey: scene.roomKey,
+        });
       });
 
       //TIMER
@@ -174,23 +169,23 @@ export default class MainScene extends Phaser.Scene {
         16,
         this.formatTime(this.initialTime),
         {
-          fontSize: "32px",
-          fill: "#ffffff",
+          fontSize: '32px',
+          fill: '#ffffff',
         }
       );
 
-      scene.startText = scene.add.text(400, 300, "START", {
-        fill: "#000000",
-        fontSize: "20px",
-        fontStyle: "bold",
+      scene.startText = scene.add.text(400, 300, 'START', {
+        fill: '#000000',
+        fontSize: '20px',
+        fontStyle: 'bold',
       });
       scene.startText.setVisible(false);
 
-      this.socket.on("destroyButton", function () {
+      this.socket.on('destroyButton', function () {
         scene.startText.destroy();
       });
 
-      this.socket.on("startTimer", function () {
+      this.socket.on('startTimer', function () {
         scene.beginTimer = Date.now();
       });
     } catch (error) {
@@ -199,6 +194,7 @@ export default class MainScene extends Phaser.Scene {
   }
 
   update(time) {
+    const scene = this;
     if (this.astronaut) {
       if (this.cursors.left.isDown) {
         this.astronaut.setVelocityX(-150);
@@ -222,9 +218,10 @@ export default class MainScene extends Phaser.Scene {
         (x !== this.astronaut.oldPosition.x ||
           y !== this.astronaut.oldPosition.y)
       ) {
-        this.socket.emit("playerMovement", {
+        this.socket.emit('playerMovement', {
           x: this.astronaut.x,
           y: this.astronaut.y,
+          roomKey: scene.roomKey,
         });
       }
 
@@ -236,12 +233,12 @@ export default class MainScene extends Phaser.Scene {
       };
     }
 
-    if (this.numPlayers >= 2 && this.startClickable === true) {
+    if (this.state.numPlayers >= 2 && this.startClickable === true) {
       this.startClickable = false;
       this.startText.setVisible(true);
       this.startText.setInteractive();
-      this.startText.on("pointerdown", () => {
-        this.startButton();
+      this.startText.on('pointerdown', () => {
+        this.socket.emit('startGame', scene.roomKey);
       });
 
       this.controlPanelLeft.setInteractive();
@@ -255,10 +252,10 @@ export default class MainScene extends Phaser.Scene {
 
   addPlayer(scene, playerInfo) {
     scene.astronaut = scene.physics.add
-      .image(playerInfo.x, playerInfo.y, "astronaut")
+      .image(playerInfo.x, playerInfo.y, 'astronaut')
       .setOrigin(0.5, 0.5)
       .setDisplaySize(43.5, 55.5);
-    if (playerInfo.team === "blue") {
+    if (playerInfo.team === 'blue') {
       scene.astronaut.setTint(0x2796a5);
     } else {
       scene.astronaut.setTint(0xd86969);
@@ -270,10 +267,10 @@ export default class MainScene extends Phaser.Scene {
 
   addOtherPlayers(scene, playerInfo) {
     const otherPlayer = scene.add
-      .sprite(playerInfo.x, playerInfo.y, "astronaut")
+      .sprite(playerInfo.x, playerInfo.y, 'astronaut')
       .setOrigin(0.5, 0.5)
       .setDisplaySize(43.5, 55.5);
-    if (playerInfo.team === "blue") {
+    if (playerInfo.team === 'blue') {
       otherPlayer.setTint(0x2796a5);
     } else {
       otherPlayer.setTint(0xd86969);
@@ -285,7 +282,7 @@ export default class MainScene extends Phaser.Scene {
   formatTime(seconds) {
     var minutes = Math.floor(seconds / 60);
     var partInSeconds = seconds % 60;
-    partInSeconds = partInSeconds.toString().padStart(2, "0");
+    partInSeconds = partInSeconds.toString().padStart(2, '0');
     return `${minutes}:${partInSeconds}`;
   }
   countdown() {
@@ -296,17 +293,14 @@ export default class MainScene extends Phaser.Scene {
       this.initialTime -= 1;
       this.timerLabel.setText(this.formatTime(this.initialTime));
       if (this.initialTime === 10) {
-        this.timerLabel.setStyle({ fill: "#ff0000" });
+        this.timerLabel.setStyle({ fill: '#ff0000' });
       }
       this.beginTimer = currentTime;
       if (this.initialTime === 0) {
         this.beginTimer = false;
-        this.scene.stop("RegexScene");
-        this.scene.launch("LoseScene");
+        this.scene.stop('RegexScene');
+        this.scene.launch('LoseScene');
       }
     }
-  }
-  startButton() {
-    this.socket.emit("startGame");
   }
 }
