71f3997300ec247f7759e784fd0defdec07950c9
hannah <contactuser2220@gmail.com>
2020-12-08 10:40:37 -0500

    feat: console on regex and waitingroom

--- src/scenes/WaitingRoom.js
+++ src/scenes/WaitingRoom.js
@@ -1,8 +1,8 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
 export default class WaitingRoom extends Phaser.Scene {
   constructor() {
-    super('WaitingRoom');
+    super("WaitingRoom");
     this.state = {};
     this.hasBeenSet = false;
   }
@@ -12,98 +12,70 @@ export default class WaitingRoom extends Phaser.Scene {
   }
 
   preload() {
-    this.load.html('codeform', 'assets/text/codeform.html');
+    this.load.html("codeform", "assets/text/codeform.html");
+    this.load.image("computer", "assets/backgrounds/computer.png");
+    this.load.image("popup", "assets/backgrounds/singlepopup.png");
   }
 
   create() {
     const scene = this;
-    // scene.socket = io();
 
-    scene.popUp = scene.add.graphics();
-    scene.boxes = scene.add.graphics();
-
-    // for popup window
-    scene.popUp.lineStyle(1, 0xffffff);
-    scene.popUp.fillStyle(0xffffff, 0.5);
-
-    // for boxes
-    scene.boxes.lineStyle(1, 0xffffff);
-    scene.boxes.fillStyle(0xa9a9a9, 1);
-
-    // popup window
-    scene.popUp.strokeRect(25, 25, 750, 500);
-    scene.popUp.fillRect(25, 25, 750, 500);
+    scene.popUp = scene.add.image(400, 300, "computer");
+    scene.requestBox = scene.add.image(300, 280, "popup");
+    scene.enterBox = scene.add.image(500, 350, "popup");
 
     //title
-    scene.title = scene.add.text(100, 75, 'RegEx Spaceship', {
-      fill: '#add8e6',
-      fontSize: '66px',
-      fontStyle: 'bold',
+    scene.title = scene.add.text(125, 83, "RegEx Spaceship", {
+      fill: "#00ff00",
+      fontSize: "32px",
+      fontStyle: "bold",
     });
 
-    //left popup
-    scene.boxes.strokeRect(100, 200, 275, 100);
-    scene.boxes.fillRect(100, 200, 275, 100);
-
     scene.requestButton = scene.add
-      .dom(
-        237.5,
-        270,
-        'button',
-        'width: 150px; height: 25px',
-        'Request Room Key'
-      )
+      .dom(280, 320, "button", "width: 150px; height: 25px", "Request Room Key")
       .setOrigin(0.5);
 
-    // scene.requestButton = scene.add.text(140, 215, "Request Room Key", {
-    //   fill: "#000000",
-    //   fontSize: "20px",
-    //   fontStyle: "bold",
-    // });
-
     //right popup
-    scene.boxes.strokeRect(425, 200, 275, 100);
-    scene.boxes.fillRect(425, 200, 275, 100);
-    scene.inputElement = scene.add.dom(562.5, 250).createFromCache('codeform');
-    scene.inputElement.addListener('click');
-    scene.inputElement.on('click', function (event) {
-      if (event.target.name === 'enterRoom') {
-        const input = scene.inputElement.getChildByName('code-form');
+    scene.inputElement = scene.add.dom(500, 400).createFromCache("codeform");
+    scene.inputElement.addListener("click");
+    scene.inputElement.on("click", function (event) {
+      if (event.target.name === "enterRoom") {
+        const input = scene.inputElement.getChildByName("code-form");
         let uppercase = input.value.replace(/[a-z]/g, (L) => L.toUpperCase());
-        scene.socket.emit('isKeyValid', uppercase);
+        scene.socket.emit("isKeyValid", uppercase);
       }
     });
 
     scene.requestButton.setInteractive();
-    scene.requestButton.on('pointerdown', () => {
-      scene.socket.emit('getRoomCode');
+    scene.requestButton.on("pointerdown", () => {
+      scene.socket.emit("getRoomCode");
       scene.requestButton.disableInteractive();
     });
 
-    scene.notValidText = scene.add.text(500, 270, '', {
-      fill: '#ff0000',
-      fontSize: '15px',
+    scene.notValidText = scene.add.text(400, 350, "", {
+      fill: "#ff0000",
+      fontSize: "15px",
     });
-    scene.roomKeyText = scene.add.text(195, 220, '', {
-      fill: '#00ff00',
-      fontSize: '30px',
-      fontStyle: 'bold',
+    scene.roomKeyText = scene.add.text(210, 250, "", {
+      fill: "#00ff00",
+      fontSize: "30px",
+      fontStyle: "bold",
     });
 
-    scene.socket.on('roomCreated', function (roomKey) {
+    scene.socket.on("roomCreated", function (roomKey) {
       scene.roomKey = roomKey;
       scene.roomKeyText.setText(scene.roomKey);
     });
-    scene.socket.on('keyNotValid', function () {
-      scene.notValidText.setText('Invalid Room Key');
+    scene.socket.on("keyNotValid", function () {
+      scene.notValidText.setText("Invalid Room Key");
     });
-    scene.socket.on('keyIsValid', function (input) {
-      scene.socket.emit('joinRoom', input);
-      scene.scene.stop('WaitingRoom');
+    scene.socket.on("keyIsValid", function (input) {
+      scene.socket.emit("joinRoom", input);
+      scene.scene.stop("WaitingRoom");
     });
-    scene.socket.on('gameAlreadyStarted', function () {
-      console.log('inside already started listener');
-      scene.notValidText.setText('Game has already begun');
+    scene.socket.on("gameAlreadyStarted", function () {
+      console.log("inside already started listener");
+      scene.notValidText.setText("Game has already begun");
     });
   }
   update() {}
