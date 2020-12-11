f55c454bd60b53be913593b6cf3c2e0916db1a11
hannah <contactuser2220@gmail.com>
2020-12-10 20:09:24 -0500

    feat: replay button when player leaves

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -369,28 +369,30 @@ export default class MainScene extends Phaser.Scene {
     this.socket.on("disconnected", function (arg) {
       const { playerId, numPlayers } = arg;
       scene.state.numPlayers = numPlayers;
-      if (scene.gameStarted) {
-        scene.progressBar.changeTaskAmount(scene.progressBar.taskAmount - 3);
-        // scene.add.text(400, 300, "Player Left: Game over");
-        // scene.playAgain = scene.add
-        //   .text(400, 500, "Play Again", {
-        //     fill: "#00ff00",
-        //     fontSize: "30px",
-        //   })
-        //   .setOrigin(0.5);
-        // scene.playAgain.setInteractive();
-        // scene.playAgain.on("pointerdown", () => {
-        //   scene.socket.emit("restartGame");
-        //   scene.sys.game.destroy(true);
-        // });
-      }
-      if (scene.otherPlayers && scene.otherPlayers.getChildren.length) {
-        scene.otherPlayers.getChildren().forEach(function (otherPlayer) {
-          if (playerId === otherPlayer.playerId) {
-            otherPlayer.destroy();
-          }
+      console.log("game started:", scene.state.gameStarted);
+      if (scene.state.gameStarted) {
+        // scene.progressBar.changeTaskAmount(scene.progressBar.taskAmount - 3);
+        scene.add.text(400, 300, "Player Left: Game over").setScrollFactor(0);
+        scene.playAgain = scene.add
+          .text(400, 500, "Play Again", {
+            fill: "#00ff00",
+            fontSize: "30px",
+          })
+          .setOrigin(0.5)
+          .setScrollFactor(0);
+        scene.playAgain.setInteractive();
+        scene.playAgain.on("pointerdown", () => {
+          scene.socket.emit("restartGame");
+          scene.sys.game.destroy(true);
         });
       }
+      // if (scene.otherPlayers && scene.otherPlayers.getChildren.length) {
+      scene.otherPlayers.getChildren().forEach(function (otherPlayer) {
+        if (playerId === otherPlayer.playerId) {
+          otherPlayer.destroy();
+        }
+      });
+      // }
     });
 
     // PROGRESS BAR UPDATE
