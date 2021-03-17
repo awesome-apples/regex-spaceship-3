3dd62967ad79750bfa6fd04b0a0192e500569e31
hannah <contactuser2220@gmail.com>
2021-03-17 17:28:43 -0400

    added ssl in db

--- server/db/db.js
+++ server/db/db.js
@@ -6,6 +6,12 @@ const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 module.exports = db;
